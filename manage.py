2edd349bbb3a60228ac5069da75578ffae2e8b0e
Damián Silvani <munshkr@gmail.com>
2019-10-20 09:21:55 -0300

    Add yapf; add and configure python-dotenv

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geolomas.settings')
@@ -12,8 +16,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "forget to activate a virtual environment?") from exc
     execute_from_command_line(sys.argv)
 
 
