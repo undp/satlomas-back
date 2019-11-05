2f84c60150f3dad66e5375f04564a931df40e3d4
Gessica <gessica@dymaxionlabs.com>
2019-11-05 11:31:50 -0300

    add imports

--- geolomas/urls.py
+++ geolomas/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
