f829a467026d399728d39a7af7c1020c6204812d
Gessica <gessica@dymaxionlabs.com>
2020-01-10 14:39:38 -0300

    error correction

--- files/tasks.py
+++ files/tasks.py
@@ -7,7 +7,7 @@ import subprocess
 from pathlib import Path
 import json
 
-# import django_rq; from datetime import date; date_from = date(2019,4,1); date_to = date(2019,5,1);queue = django_rq.get_queue('default', default_timeout=36000);queue.enqueue("files.tasks.download_sentinel2", date_from, date_to);w = django_rq.get_worker(); w.work()
+# import django_rq; from datetime import date; date_from = date(2019,4,1); date_to = date(2019,4,10);queue = django_rq.get_queue('default', default_timeout=36000);queue.enqueue("files.tasks.download_sentinel2", date_from, date_to);w = django_rq.get_worker(); w.work()
 
 def run_subprocess(cmd):
     print(cmd)
@@ -63,12 +63,8 @@ def download_sentinel2(date_from, date_to):
             return_values.append(rv)
 
     # si fallan todas las imagenes de sen2cor, levantar excepcion
-    error = True
-    for rv in return_values:
-        if rv == 0:
-            error = False
-            break
-    if error:
+    error = all([rv == 0 for rv in return_values])
+    if error == False:
         raise ValueError('All sen2cor images failed.')
 
     # obtain necesary gdal info
@@ -101,7 +97,7 @@ def download_sentinel2(date_from, date_to):
     if gdal_info:
         cmd = "python3 {} -te {} {} {} {} -e 32718 -res 20 -n {} -v -o {} {}".format(
             settings.S2M_PATH, xmin, ymin, xmax, ymax, "name_of_mosaic", os.path.join(settings.IMAGES_PATH,"results"), settings.IMAGES_PATH
-        )|
+        )
         run_subprocess(cmd)
         rv = os.system(cmd)
         # si return value != 0, s2m falló, generar excepcion
