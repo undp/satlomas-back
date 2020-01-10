8cf89e8d8db6d67bd51235285429baac889750a4
alan <alan@debian>
2020-01-10 16:20:25 -0300

    change tasks to files

--- files/models.py
+++ files/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Period(models.Model):
+    init_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return '{init_date}_{end_date}'.format(init_date=self.init_date,
+                                                end_date=self.end_date)
+
+
+class Product(models.Model):
+    code = models.CharField(max_length=128)
+    period = models.ForeignKey(Period, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=128)
+    datetime = models.DateTimeField(null=True)
+    concatenated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
