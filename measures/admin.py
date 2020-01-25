e96520e0fa96d010ed1a9a0feeb9b970c6f73bef
Damián Silvani <munshkr@gmail.com>
2020-01-25 10:49:23 -0300

    Register Place and Station on admin

--- measures/admin.py
+++ measures/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Device
 
+from .models import Device, Place, Station
 
 admin.site.register(Device)
+admin.site.register(Place)
+admin.site.register(Station)
