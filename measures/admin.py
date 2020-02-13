ac75c486b05dcbf45806c276a2e61e6be2dbd8c2
Damián Silvani <munshkr@gmail.com>
2020-02-13 15:08:52 -0300

    Use LeafletGeoAdmin for Place and Station models

--- measures/admin.py
+++ measures/admin.py
@@ -1,11 +1,7 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from leaflet.admin import LeafletGeoAdmin
 
 from .models import Place, Station
 
-
-class StationAdmin(admin.ModelAdmin):
-    exclude = ('geom', )
-
-
-admin.site.register(Place)
-admin.site.register(Station, StationAdmin)
+admin.site.register(Place, LeafletGeoAdmin)
+admin.site.register(Station, LeafletGeoAdmin)
