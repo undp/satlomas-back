1de00e851e6d00a3cd5d4436b6d9f183f8255601
alan <alan@debian>
2020-03-06 11:18:48 -0300

    Fix manager sql for adapting to new columns

--- measures/managers.py
+++ measures/managers.py
@@ -5,48 +5,70 @@ class MeasureManager(models.Manager):
         from django.db import connection
         with connection.cursor() as cursor:
             cursor.execute("""
-                SELECT m.datetime, m.temperature, m.humidity, m.device_id
+                SELECT m.datetime, m.station, m.temperature, m.humidity, m.win_speed, m.win_direction,
+                m.pressure, m.precipitation, m.pm25
                 FROM measures_measure m
                 ORDER BY m.datetime DESC""")
             result_list = []
             for row in cursor.fetchall():
-                m = self.model(datetime=row[0], temperature=row[1], humidity=row[2], device_id=row[3])
+                m = self.model(datetime=row[0], station=row[1], 
+                                temperature=row[2], humidity=row[3],
+                                win_spped=row[4], win_direction=row[5],
+                                pressure=row[6], precipitation=row[7],
+                                pm25=row[8])
                 result_list.append(m)
         return result_list
 
-    def create(self, datetime, temperature, humidity, device_id):
+    def create(self, datetime, station, temperature, humidity, win_speed, win_direction, pressure, precipitation, pm25):
         from django.db import connection
         with connection.cursor() as cursor:
             cursor.execute("""
-                INSERT INTO measures_measure(datetime, temperature, humidity, device_id)
-                VALUES ('{datetime}', {temperature}, {humidity}, '{device_id}');
+                INSERT INTO measures_measure(datetime, station, temperature, humidity, win_speed,
+                win_direction, pressure, precipitation, pm25)
+                VALUES ('{datetime}', '{station}', {temperature}, {humidity}, {win_speed}, 
+                {win_direction}, {pressure}, {precipitation}, {pm25});
             """.format(
                 datetime=str(datetime),
+                station=station,
                 temperature=temperature,
                 humidity=humidity,
-                device_id=device_id
+                win_speed=win_speed,
+                win_direction=win_direction,
+                pressure=pressure,
+                precipitation=precipitation,
+                pm25=pm25
             ))
             return self.model(
                 datetime=datetime, 
-                temperature=temperature, 
-                humidity=humidity, 
-                device_id=device_id
+                station=station,
+                temperature=temperature,
+                humidity=humidity,
+                win_speed=win_speed,
+                win_direction=win_direction,
+                pressure=pressure,
+                precipitation=precipitation,
+                pm25=pm25
             )
 
-    def get(self, datetime, device_id):
+    def get(self, datetime, station):
         from django.db import connection
         with connection.cursor() as cursor:
             cursor.execute("""
-            SELECT m.datetime, m.temperature, m.humidity, m.device_id
+            SELECT m.datetime, m.station, m.temperature, m.humidity, m.win_speed, m.win_direction,
+                m.pressure, m.precipitation, m.pm25
             FROM measures_measure m
-            where m.datetime = '{datetime}' and m.device_id = '{device_id}'
+            where m.datetime = '{datetime}' and m.station = '{station}'
             """.format(
                 datetime=str(datetime),
-                device_id=device_id
+                station=station
             ))
             result_list = []
             for row in cursor.fetchall():
-                m = self.model(datetime=row[0], temperature=row[1], humidity=row[2], device_id=row[3])
+                m = self.model(datetime=row[0], station=row[1], 
+                                temperature=row[2], humidity=row[3],
+                                win_spped=row[4], win_direction=row[5],
+                                pressure=row[6], precipitation=row[7],
+                                pm25=row[8])
                 result_list.append(m)
         if len(result_list) == 0:
             raise Exception("There is no results for that query")
