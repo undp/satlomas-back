a9bf911d031f76f80bcbb6b389c387fd0ccc6509
alan <alan@debian>
2020-03-17 14:00:45 -0300

    initial model vegetation mask

--- vegetation/models.py
+++ vegetation/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+class VegetationMask(models.Model):
+    period = models.DateField()
+    mask = models.MultiPolygonField()
