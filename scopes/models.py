ed000ad9989c68580e23f17a456d59ce9b1a98a3
alan <alan@debian>
2020-03-18 14:27:23 -0300

    Initial Scope scripts

--- scopes/models.py
+++ scopes/models.py
@@ -4,28 +4,83 @@ from django.contrib.gis.geos import GEOSGeometry
 
 
 class Scope(models.Model):
-    CORREDORES = 'CO'
-    TYPE_SCOPES = [
-        (CORREDORES, 'Corredores'),
+    CORREDORES = 'CE'
+    ACR = 'AC'
+    DISTRITOS = 'DI'
+    ECOSISTEMAS = 'EF'
+    ARQUEOLOGICOS = 'SA'
+    SCOPE_TYPE = [
+        (CORREDORES, 'Corredores Ecologicos'),
+        (ACR, 'ACR'),
+        (DISTRITOS, 'Distritos'),
+        (ECOSISTEMAS, 'Ecosistemas fragiles'),
+        (ARQUEOLOGICOS, 'Sitios arqueologicos'),
     ]
-    type_scope = models.CharField(
+    scope_type = models.CharField(
         max_length=2,
-        choices=TYPE_SCOPES,
+        choices=SCOPE_TYPE,
     )
-    period = models.DateField()
-    poly = models.MultiPolygonField()
+    geom = models.MultiPolygonField()
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.scope_type)
 
     #objects = models.GeoManager()
     @classmethod
-    def save_from_geojson(cls, geojson_path, name, period, type_scope):
-        ds = DataSource(geojson_path)
-        feature = ds[0][0]
-        wkt = feature.geom.wkt
-        mp = GEOSGeometry(wkt)
-        return cls.objects.create(
-            type_scope=type_scope,
-            period=period,
-            name=name,
-            poly=mp
-        )
+    def save_scope(cls, path, scope_type = None):
+        """Save scopes from geojson
+
+        Parameters
+        ----------
+        path : str
+            The geojson path
+
+        scope_type : str, optional
+            Scope type, if None it will try to be completed by scope name 
+        """
+        import os
+        from django.contrib.gis.gdal import DataSource
+        from django.contrib.gis.geos import GEOSGeometry
+
+        if scope_type is None:
+            scope_type = ""
+            f = os.path.basename(path)
+            if f.startswith("acr"):
+                scope_type = "AC"
+            elif f.startswith("ecosistemas"):
+                scope_type = 'EF'
+            elif f.startswith("corredores"):
+                scope_type = 'CE'
+            elif f.startswith("distritos"):
+                scope_type = 'DS'
+            elif f.startswith('sitios'):
+                scope_type = 'SA'
+            else:
+                Exception("File doesn't match with any scope type")
+        
+        ds = DataSource(path)
+        for x in range(0,len(ds[0])):
+            feature = ds[0][x]
+            mp = GEOSGeometry(feature.geom.wkt)
+            return cls.objects.create(
+                scope_type=scope_type,
+                name=str(ds[0][x]['name']),
+                geom=mp
+            )
+
+    @classmethod
+    def initial_load(cls, path):
+        """Initial load from geojson scopes
+
+        Parameters
+        ----------
+        path : str
+            The folder path that contains geojson scopes
+        """
+        import os
+        from os.path import isfile, join
+        scopes_files = [f for f in os.listdir(path) if isfile(join(path, f))]
+        for f in scopes_files:
+            cls.save_scope(join(path, f))
+
