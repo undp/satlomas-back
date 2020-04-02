da56e76f7be26d9176c6e55a5685b203e345cf81
Damián Silvani <munshkr@gmail.com>
2020-04-02 12:10:38 -0300

    Define bulk_create on MeasureManager

--- measures/managers.py
+++ measures/managers.py
@@ -31,6 +31,19 @@ class MeasureManager(models.Manager):
                               station_id=station_id,
                               attributes=attributes)
 
+    def bulk_create(self, objs):
+        with connection.cursor() as cursor:
+            values = ', '.join([
+                "('{datetime}', '{station_id}', '{attributes}')".format(
+                    datetime=str(o.datetime),
+                    station_id=o.station_id,
+                    attributes=json.dumps(o.attributes)) for o in objs
+            ])
+            cursor.execute("""
+                INSERT INTO measures_measure(datetime, station_id, attributes)
+                VALUES {values};
+            """.format(values=values))
+
     def get(self, datetime, station_id):
         with connection.cursor() as cursor:
             cursor.execute("""
