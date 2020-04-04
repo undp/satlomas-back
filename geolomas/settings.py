b83317ab47830e82f3c7c4b530fd2ae81561d961
Leandro Abraham <leandromaf@gmail.com>
2020-04-04 21:11:12 +0000

    LSTM training django command
    Taking as example the scripts and the module from geolomas-exp we implement a django command to train an LSTM Neural network for time series prediction but in this case using the data from postgres and the django managers

--- geolomas/settings.py
+++ geolomas/settings.py
@@ -179,6 +179,7 @@ MEDIA_URL = '/uploads/'
 JSON_EDITOR_JS = 'https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/4.2.1/jsoneditor.js'
 JSON_EDITOR_CSS = 'https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/4.2.1/jsoneditor.css'
 
+CONFIG_DIR = os.path.join(BASE_DIR, 'config')
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 IMAGES_PATH = os.path.join(DATA_DIR, 'images', 's2')
 IMAGES_PATH_S1 = os.path.join(DATA_DIR, 'images', 's1', 'raw')
