8b3c54dc07237c6b6435ee2be107aa0f564073f6
Damián Silvani <munshkr@gmail.com>
2020-04-14 12:31:07 -0300

    alerts: Create rule models

--- alerts/admin.py
+++ alerts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ParameterRule, ScopeRule, ScopeTypeRule
+
+admin.site.register(ParameterRule)
+admin.site.register(ScopeRule)
+admin.site.register(ScopeTypeRule)
--- alerts/models.py
+++ alerts/models.py
@@ -1,5 +1,57 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
 from django.db import models
 
+from scopes.models import Scope
 
-class Alert(models.Model):
-    pass
+# FIXME Move this to Settings
+CHANGES_APPS = ['lomas_changes', 'vi_lomas_changes']
+# FIXME generate based on CHANGES_APPS
+LIMITS = models.Q(
+    app_label='lomas_changes', model='coverage_measurements') | models.Q(
+        app_label='vi_lomas_changes', model='coverage_measurements')
+
+THRESHOLD_TYPES = [
+    ('A', 'Area'),
+    ('P', 'Percentage'),
+]
+
+
+class ScopeTypeRule(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    scope_type = models.CharField(max_length=2, choices=Scope.SCOPE_TYPE)
+    measurement_content_type = models.ForeignKey(ContentType,
+                                                 on_delete=models.CASCADE,
+                                                 limit_choices_to=LIMITS)
+    threshold_type = models.CharField(max_length=1, choices=THRESHOLD_TYPES)
+    threshold = models.FloatField(default=5)
+
+
+class ScopeRule(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    scope = models.ForeignKey('scopes.Scope', on_delete=models.CASCADE)
+    measurement_content_type = models.ForeignKey(ContentType,
+                                                 on_delete=models.CASCADE,
+                                                 limit_choices_to=LIMITS)
+    threshold_type = models.CharField(max_length=1, choices=THRESHOLD_TYPES)
+    threshold = models.FloatField(default=5)
+
+
+class ParameterRule(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    parameter = models.CharField(max_length=64)
+    threshold = models.FloatField(default=5)
+
+
+# class Alert(models.Model):
+#     rule_content_type = models.ForeignKey(ContentType,
+#                                           on_delete=models.CASCADE)
+#     rule_id = models.PositiveIntegerField()
+#     rule = GenericForeignKey('rule_content_type', 'rule_id')
+
+#     measurement_content_type = models.ForeignKey(ContentType,
+#                                                  on_delete=models.CASCADE)
+#     measurement_id = models.PositiveIntegerField()
+#     measurement = GenericForeignKey('measurement_content_type',
+#                                     'measurement_id')
