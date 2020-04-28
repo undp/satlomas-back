3e177d9fd7c55b60b4f18458473afee797726740
alan <alan@debian>
2020-04-28 12:42:51 -0300

    chante data visualization from serialilzers

--- alerts/serializers.py
+++ alerts/serializers.py
@@ -8,12 +8,21 @@ class ParameterRuleSerializer(serializers.ModelSerializer):
 
 
 class ScopeRuleSerializer(serializers.ModelSerializer):
+    change_type = serializers.CharField(source='get_change_type_display')
+
     class Meta:
         model = ScopeRule
         exclude = ('user', )
 
 
 class ScopeTypeRuleSerializer(serializers.ModelSerializer):
+    change_type = serializers.CharField(source='get_change_type_display')
+    scope_type = serializers.CharField(source='get_scope_type_display')
+    measurement_content_type = serializers.SerializerMethodField('get_measurement_content_type')
+
+    def get_measurement_content_type(self, instance):
+        return instance.measurement_content_type.app_label
+
     class Meta:
         model = ScopeTypeRule
         exclude = ('user', )
