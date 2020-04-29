cabc5e11daf1ebb6fb6f6a89cbfb1686810d78a7
alan <alan@debian>
2020-04-29 11:38:12 -0300

    user filter in list querys

--- alerts/views.py
+++ alerts/views.py
@@ -11,17 +11,29 @@ class ParameterRuleViewSet(viewsets.ModelViewSet):
     queryset = ParameterRule.objects.all().order_by('-created_at')
     serializer_class = ParameterRuleSerializer
 
+    def get_queryset(self):
+        return ParameterRule.objects.filter(user=self.request.user).order_by('-created_at')
+
 
 class ScopeRuleViewSet(viewsets.ModelViewSet):
     queryset = ScopeRule.objects.all().order_by('-created_at')
     serializer_class = ScopeRuleSerializer
+
+    def get_queryset(self):
+        return ScopeRule.objects.filter(user=self.request.user).order_by('-created_at')
 
 
 class ScopeTypeRuleViewSet(viewsets.ModelViewSet):
     queryset = ScopeTypeRule.objects.all().order_by('-created_at')
     serializer_class = ScopeTypeRuleSerializer
 
+    def get_queryset(self):
+        return ScopeTypeRule.objects.filter(user=self.request.user).order_by('-created_at')
+
 
 class AlertViewSet(viewsets.ModelViewSet):
     queryset = Alert.objects.all().order_by('created_at')
     serializer_class = AlertSerializer
+
+    def get_queryset(self):
+        return Alert.objects.filter(user=self.request.user).order_by('-created_at')
