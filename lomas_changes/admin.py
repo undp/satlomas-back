1a2045bf4390af144f159e0c32674f0e94174faa
Damián Silvani <munshkr@gmail.com>
2020-05-05 23:39:26 -0300

    Define RasterAdmin for default ordering

--- lomas_changes/admin.py
+++ lomas_changes/admin.py
@@ -4,12 +4,17 @@ from leaflet.admin import LeafletGeoAdmin
 from .models import CoverageMeasurement, Mask, Period, Raster
 
 
+class RasterAdmin(admin.ModelAdmin):
+    date_hierarchy = 'period__date_to'
+    ordering = ('-period__date_from', '-period__date_to')
+
+
 class PeriodAdmin(admin.ModelAdmin):
     readonly_fields = (
         'date_from',
         'date_to',
     )
-    date_hierarchy = 'date_from'
+    date_hierarchy = 'date_to'
     ordering = ('-date_from', '-date_to')
 
 
@@ -31,7 +36,7 @@ class CoverageMeasurementAdmin(admin.ModelAdmin):
     ordering = ('-created_at', )
 
 
-admin.site.register(Raster)
+admin.site.register(Raster, RasterAdmin)
 admin.site.register(Period, PeriodAdmin)
 admin.site.register(Mask, MaskAdmin)
 admin.site.register(CoverageMeasurement, CoverageMeasurementAdmin)
