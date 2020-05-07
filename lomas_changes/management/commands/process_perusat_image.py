2fd9dd8933ef69807ae23bf952e84c3a50c892d1
Damián Silvani <munshkr@gmail.com>
2020-05-07 16:49:02 -0300

    Define command for loading PS1 images and results

---
+++ lomas_changes/management/commands/process_perusat_image.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+
+from lomas_changes.models import Period
+from lomas_changes.tasks.perusat1 import load_data
+
+
+class Command(BaseCommand):
+    help = 'Loads an already processed PeruSat-1 image and object detection results'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--product-id', required=True)
+        parser.add_argument('--date',
+                            type=lambda s: datetime.strptime(s, '%Y-%m-%d'),
+                            required=True)
+
+    def handle(self, *args, **options):
+        date = options['date'].date()
+
+        period, _ = Period.objects.get_or_create(date_from=date, date_to=date)
+        load_data(period)
