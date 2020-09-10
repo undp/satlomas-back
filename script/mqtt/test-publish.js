4bc856153093b11ea7550d1eee2e8b5d73f4bf16
Damián Silvani <munshkr@gmail.com>
2020-09-10 12:19:20 -0300

    Update MQTT publish test script with real parameters

--- script/mqtt/test-publish.js
+++ script/mqtt/test-publish.js
@@ -1,22 +1,44 @@
 #!/usr/bin/env node
-require('dotenv').config()
+require("dotenv").config();
 
-const process = require('process');
-const mqtt = require('mqtt');
+const process = require("process");
+const mqtt = require("mqtt");
 
 const mqttUrl = process.env.MQTT_URL;
-const mqttPath = process.env.MQTT_PATH;
+
+const randFloat = (min, max, prec) => {
+  return (Math.random() * (max - min) + min).toFixed(prec);
+};
+
+const randInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 const client = mqtt.connect(mqttUrl, {
-  clientId: 'test'
+  clientId: "test",
 });
 
-client.on('connect', () => {
-  let i = 0;
+client.on("connect", () => {
+  let i = 1;
 
   setInterval(() => {
-    const data = { type: 'simple', message: 'this is a test', index: i };
+    const data = {
+      altitude: randFloat(70, 85, 2),
+      ambient_temperature: randFloat(15, 25, 2),
+      atmospheric_pressure: randFloat(800, 1200, 2),
+      internal_temperature: randFloat(16, 28, 2),
+      PM1_0: randInt(20, 40),
+      PM2_5: randInt(20, 40),
+      PM4_0: randInt(20, 40),
+      PM10_0: randInt(20, 40),
+      relative_humidity: randFloat(0, 100, 2),
+      time: Date.now().toISOString(),
+      tip_count: randInt(0, 3000),
+    };
+
+    const mqttPath = `/stations/${id % 3}`;
     client.publish(mqttPath, JSON.stringify(data));
+
     i += 1;
   }, 1000);
 });
