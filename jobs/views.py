c3845de6b6f44d47063eae71451037761b662658
Damián Silvani <munshkr@gmail.com>
2020-09-24 15:42:20 -0300

    Configure pagination on jobs viewsets

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from jobs.models import Job, JobLogEntry
 from jobs.serializers import JobLogEntrySerializer, JobSerializer
@@ -8,11 +9,13 @@ from jobs.serializers import JobLogEntrySerializer, JobSerializer
 class JobViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Job.objects.all().order_by('-finished_at', '-created_at')
     serializer_class = JobSerializer
+    pagination_class = PageNumberPagination
 
 
 class JobLogEntryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = JobLogEntry.objects.all().order_by('-logged_at')
     serializer_class = JobLogEntrySerializer
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         return self.queryset.filter(job=self.kwargs['job_id'])
