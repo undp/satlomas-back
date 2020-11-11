5f3dea7cf4c183a355b675845ce63c97e7b1f2b2
Damián Silvani <munshkr@gmail.com>
2020-11-11 12:31:38 -0300

    lomas_changes: Only unzip if dir does not exist

--- lomas_changes/tasks/sentinel2.py
+++ lomas_changes/tasks/sentinel2.py
@@ -117,7 +117,9 @@ def download_and_build_composite(date_from, date_to):
     # Unzip compressed files, if there are any
     for p in glob(os.path.join(raw_dir, '*.zip')):
         logger.info("Unzip %s", p)
-        unzip(p, delete_zip=False)
+        p_dir = os.path.join(raw_dir, f'{os.path.basename(p)}.SAFE')
+        if not os.path.exists(p_dir):
+            unzip(p, delete_zip=False)
 
     # Build mosaic
     proc_scene_dir = os.path.join(PROC_DIR, period_s)
