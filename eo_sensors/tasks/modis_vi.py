c49a0f5fbc395d75cdbab10a3357a7c6aff34699
Damián Silvani <munshkr@gmail.com>
2021-04-25 17:07:13 -0300

    eo_sensors: Uncomment line

--- eo_sensors/tasks/modis_vi.py
+++ eo_sensors/tasks/modis_vi.py
@@ -95,7 +95,7 @@ def process_period(job):
     date_from = datetime.strptime(job.kwargs["date_from"], "%Y-%m-%d")
     date_to = datetime.strptime(job.kwargs["date_to"], "%Y-%m-%d")
 
-    # download_and_process(date_from, date_to)
+    download_and_process(date_from, date_to)
     create_rgb_rasters(date_from, date_to)
     create_masks(date_from, date_to)
     generate_measurements(date_to)
