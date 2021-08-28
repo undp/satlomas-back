c8874240d4c4719838b8da2edeb872a1f29c40cd
Damián Silvani <munshkr@gmail.com>
2021-08-27 23:27:06 -0300

    stations: Replace PointField for a LatLongWidget on Station admin

--- stations/admin.py
+++ stations/admin.py
@@ -1,14 +1,16 @@
 from django.contrib.gis import admin
+from django.contrib.gis.db.models import PointField
 from django.db.models import JSONField
 from jsoneditor.forms import JSONEditor
-from leaflet.admin import LeafletGeoAdmin
 
 from .models import Station
+from .widgets import LatLongWidget
 
 
-class StationAdmin(LeafletGeoAdmin):
+class StationAdmin(admin.ModelAdmin):
     list_display = ("code", "name", "geom", "created_at", "updated_at")
     formfield_overrides = {
+        PointField: {"widget": LatLongWidget},
         JSONField: {"widget": JSONEditor},
     }
 
