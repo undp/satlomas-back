19fb31808c1c800b2d14b1b8714af4793aab6fba
Damián Silvani <munshkr@gmail.com>
2021-09-04 14:00:16 -0300

    eo_sensors: Re-format perusat1 task file

--- eo_sensors/tasks/perusat1.py
+++ eo_sensors/tasks/perusat1.py
@@ -17,43 +17,43 @@ from jobs.utils import enqueue_job, job
 # Configure logger
 logger = logging.getLogger(__name__)
 out_handler = logging.StreamHandler(sys.stdout)
-out_handler.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+out_handler.setFormatter(logging.Formatter("%(asctime)s %(message)s"))
 out_handler.setLevel(logging.INFO)
 logger.addHandler(out_handler)
 logger.setLevel(logging.INFO)
 
 # Base directory
-DATA_DIR = os.path.join(settings.DATA_DIR, 'eo_sensors', 'ps1')
+DATA_DIR = os.path.join(settings.DATA_DIR, "eo_sensors", "ps1")
 # "raw" directory contains uncompressed Level-1 PeruSat-1 scenes
-RAW_DIR = os.path.join(DATA_DIR, 'raw')
+RAW_DIR = os.path.join(DATA_DIR, "raw")
 # "proc" directory contains pansharpened scenes (result of perusatproc)
-PROC_DIR = os.path.join(DATA_DIR, 'proc')
+PROC_DIR = os.path.join(DATA_DIR, "proc")
 # "chips" dreictory contains all image chips for prediction
-CHIPS_DIR = os.path.join(DATA_DIR, 'chips')
+CHIPS_DIR = os.path.join(DATA_DIR, "chips")
 # "predict" directory contains result chips from prediction
-PREDICT_DIR = os.path.join(DATA_DIR, 'predict')
+PREDICT_DIR = os.path.join(DATA_DIR, "predict")
 # "results" directory contains final classification result as RGB raster
 # (using a colormap).
-RESULTS_DIR = os.path.join(DATA_DIR, 'results')
+RESULTS_DIR = os.path.join(DATA_DIR, "results")
 
-AOI_PATH = os.path.join(DATA_DIR, 'aoi.gpkg')
+AOI_PATH = os.path.join(DATA_DIR, "aoi.gpkg")
 
 RESCALE_RANGE = ((100, 275), (110, 250), (120, 220))
 BANDS = (1, 2, 3)  # RGB
-CLASSES = ('C', 'U', 'D')
+CLASSES = ("C", "U", "D")
 SIZE = 320
 STEP_SIZE = 160
 BATCH_SIZE = 32
-MODEL_PATH = os.path.join(DATA_DIR, 'weights', 'lomas_ps1_v3.h5')
+MODEL_PATH = os.path.join(DATA_DIR, "weights", "lomas_ps1_v3.h5")
 BIN_THRESHOLD = 0.4
 
 
-@job('processing')
+@job("processing")
 def import_scene_from_sftp(job):
     """Connects to an SFTP server and downloads a scene"""
 
-    sftp_conn_info = job.kwargs['sftp_conn_info']
-    filepath = job.kwargs['file']
+    sftp_conn_info = job.kwargs["sftp_conn_info"]
+    filepath = job.kwargs["file"]
 
     client = SFTPClient(**sftp_conn_info)
     with tempfile.TemporaryDirectory() as tmpdir:
@@ -68,14 +68,16 @@ def import_scene_from_sftp(job):
         logger.info("Unzip %s into %s", dst, scene_dir)
         unzip(dst, scene_dir)
 
-        enqueue_job('eo_sensors.tasks.perusat1.pansharpen_scene',
-                    scene_dir=scene_dir,
-                    queue='processing')
+        enqueue_job(
+            "eo_sensors.tasks.perusat1.pansharpen_scene",
+            scene_dir=scene_dir,
+            queue="processing",
+        )
 
 
-@job('processing')
+@job("processing")
 def pansharpen_scene(job):
-    raw_scene_dir = job.kwargs['scene_dir']
+    raw_scene_dir = job.kwargs["scene_dir"]
 
     from perusatproc.console.process import process_product
 
@@ -88,95 +90,110 @@ def pansharpen_scene(job):
     logger.info("Delete raw scene directory")
     shutil.rmtree(raw_scene_dir)
 
-    enqueue_job('eo_sensors.tasks.perusat1.extract_chips_from_scene',
-                scene_dir=proc_scene_dir,
-                queue='processing')
+    enqueue_job(
+        "eo_sensors.tasks.perusat1.extract_chips_from_scene",
+        scene_dir=proc_scene_dir,
+        queue="processing",
+    )
 
 
-@job('processing')
+@job("processing")
 def extract_chips_from_scene(job):
-    scene_dir = job.kwargs['scene_dir']
+    scene_dir = job.kwargs["scene_dir"]
 
     from satlomasproc.chips import extract_chips
 
-    rasters = glob(os.path.join(scene_dir, '*.tif'))
+    rasters = glob(os.path.join(scene_dir, "*.tif"))
     logger.info("Num. rasters: %i", len(rasters))
 
     chips_dir = os.path.join(CHIPS_DIR, os.path.basename(scene_dir))
-    logger.info("Extract chips on images from %s into %s", scene_dir,
-                chips_dir)
+    logger.info("Extract chips on images from %s into %s", scene_dir, chips_dir)
 
     # TODO: Generate RGB image
     # TODO: Load RGB image
 
     # FIXME: Once RGB image is prepared, there is no need to rescale on chips...
-    extract_chips(rasters,
-                  aoi=AOI_PATH,
-                  rescale_mode='values',
-                  rescale_range=RESCALE_RANGE,
-                  bands=BANDS,
-                  type='tif',
-                  size=SIZE,
-                  step_size=STEP_SIZE,
-                  output_dir=chips_dir)
+    extract_chips(
+        rasters,
+        aoi=AOI_PATH,
+        rescale_mode="values",
+        rescale_range=RESCALE_RANGE,
+        bands=BANDS,
+        type="tif",
+        size=SIZE,
+        step_size=STEP_SIZE,
+        output_dir=chips_dir,
+    )
 
-    enqueue_job('eo_sensors.tasks.perusat1.predict_scene',
-                chips_dir=chips_dir,
-                queue='processing')
+    enqueue_job(
+        "eo_sensors.tasks.perusat1.predict_scene",
+        chips_dir=chips_dir,
+        queue="processing",
+    )
 
 
-@job('processing')
+@job("processing")
 def predict_scene(job):
-    chips_dir = job.kwargs['chips_dir']
+    chips_dir = job.kwargs["chips_dir"]
 
     from satlomasproc.unet.predict import PredictConfig, predict
 
     predict_chips_dir = os.path.join(PREDICT_DIR, os.path.basename(chips_dir))
-    cfg = PredictConfig(images_path=chips_dir,
-                        results_path=predict_chips_dir,
-                        batch_size=BATCH_SIZE,
-                        model_path=MODEL_PATH,
-                        height=SIZE,
-                        width=SIZE,
-                        n_channels=len(BANDS),
-                        n_classes=len(CLASSES))
+    cfg = PredictConfig(
+        images_path=chips_dir,
+        results_path=predict_chips_dir,
+        batch_size=BATCH_SIZE,
+        model_path=MODEL_PATH,
+        height=SIZE,
+        width=SIZE,
+        n_channels=len(BANDS),
+        n_classes=len(CLASSES),
+    )
     logger.info("Predict chips on %s", predict_chips_dir)
     predict(cfg)
 
     logger.info("Delete chips directory")
     shutil.rmtree(chips_dir)
 
-    enqueue_job('eo_sensors.tasks.perusat1.postprocess_scene',
-                predict_chips_dir=predict_chips_dir,
-                queue='processing')
+    enqueue_job(
+        "eo_sensors.tasks.perusat1.postprocess_scene",
+        predict_chips_dir=predict_chips_dir,
+        queue="processing",
+    )
 
 
-@job('processing')
+@job("processing")
 def postprocess_scene(job):
-    predict_chips_dir = job.kwargs['predict_chips_dir']
+    predict_chips_dir = job.kwargs["predict_chips_dir"]
 
-    from satlomasproc.unet.postprocess import (clip, coalesce_and_binarize_all,
-                                           merge_all)
+    from satlomasproc.unet.postprocess import clip, coalesce_and_binarize_all, merge_all
 
-    result_path = os.path.join(RESULTS_DIR,
-                               f'{os.path.basename(predict_chips_dir)}.tif')
+    result_path = os.path.join(
+        RESULTS_DIR, f"{os.path.basename(predict_chips_dir)}.tif"
+    )
 
     with tempfile.TemporaryDirectory() as tmpdir:
-        bin_path = os.path.join(tmpdir, 'bin')
+        bin_path = os.path.join(tmpdir, "bin")
         logger.info(
             "Coalesce and binarize all in %s into %s (with threshold %d)",
-            predict_chips_dir, bin_path, BIN_THRESHOLD)
-        coalesce_and_binarize_all(input_dir=predict_chips_dir,
-                                  output_dir=bin_path,
-                                  threshold=BIN_THRESHOLD)
+            predict_chips_dir,
+            bin_path,
+            BIN_THRESHOLD,
+        )
+        coalesce_and_binarize_all(
+            input_dir=predict_chips_dir, output_dir=bin_path, threshold=BIN_THRESHOLD
+        )
 
-        merged_path = os.path.join(tmpdir, 'merged.tif')
-        logger.info("Merge all binarized chips on %s into %s", bin_path,
-                    merged_path)
+        merged_path = os.path.join(tmpdir, "merged.tif")
+        logger.info("Merge all binarized chips on %s into %s", bin_path, merged_path)
         merge_all(input_dir=bin_path, output=merged_path)
 
-        logger.info("Clip merged raster %s into %s using AOI at %s",
-                    merged_path, result_path, AOI_PATH)
+        logger.info(
+            "Clip merged raster %s into %s using AOI at %s",
+            merged_path,
+            result_path,
+            AOI_PATH,
+        )
         clip(src=merged_path, dst=result_path, aoi=AOI_PATH)
 
     logger.info("Delete predict chips")
@@ -184,16 +201,16 @@ def postprocess_scene(job):
 
 
 def load_data(period, product_id):
-    #create_rgb_rasters(period, product_id)
+    # create_rgb_rasters(period, product_id)
     load_raster(period, product_id)
     load_mask_and_objects(period, product_id)
 
 
 def load_raster(period, product_id):
     logger.info(f"Load {product_id} raster")
-    Raster.objects.update_or_create(period=period,
-                                    slug='ps1',
-                                    defaults=dict(name=product_id))
+    Raster.objects.update_or_create(
+        period=period, slug="ps1", defaults=dict(name=product_id)
+    )
 
 
 def load_mask_and_objects(period, product_id):
@@ -202,11 +219,11 @@ def load_mask_and_objects(period, product_id):
     from shapely.ops import unary_union
 
     logging.info("Reproject to epsg:4326")
-    src_path = os.path.join(RESULTS_DIR, product_id, 'objects.geojson')
-    dst_path = os.path.join(RESULTS_DIR, product_id, 'objects_4326.geojson')
+    src_path = os.path.join(RESULTS_DIR, product_id, "objects.geojson")
+    dst_path = os.path.join(RESULTS_DIR, product_id, "objects_4326.geojson")
     data = gpd.read_file(src_path)
     data_proj = data.copy()
-    data_proj['geometry'] = data_proj['geometry'].to_crs(epsg=4326)
+    data_proj["geometry"] = data_proj["geometry"].to_crs(epsg=4326)
     data_proj.to_file(dst_path)
 
     logger.info("Load roofs mask to DB")
@@ -218,11 +235,12 @@ def load_mask_and_objects(period, product_id):
     multipoly = unary_union(polys)
     Mask.objects.update_or_create(
         period=period,
-        mask_type='roofs',
-        defaults=dict(geom=GEOSGeometry(multipoly.wkt)))
+        mask_type="roofs",
+        defaults=dict(geom=GEOSGeometry(multipoly.wkt)),
+    )
 
     Object.objects.filter(period=period).delete()
     for poly in polys:
-        Object.objects.create(period=period,
-                              object_type="roof",
-                              geom=GEOSGeometry(poly.wkt))
+        Object.objects.create(
+            period=period, object_type="roof", geom=GEOSGeometry(poly.wkt)
+        )
